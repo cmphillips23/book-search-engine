f2ce46ea1ac272d52d80f315fc65b64370d0ebd1
Cnidarian <cmphillips23@gmail.com>
2021-06-14 22:58:45 -0500

    added queries and mutations to client, and changed loginform and signupform to use mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,62 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql `
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                email
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql `
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql `
+    mutation saveBook($input: BookInput) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                title
+                bookId
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql `
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                title
+                bookId
+                image
+                link
+            }
+        }
+    }
+`;
